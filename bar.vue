5b34d3bfb8fcfe220bb61593321cf6e33bc1b05e
Pvwel <118042769+Pvwel@users.noreply.github.com>
2022-11-12 09:04:35 +0100

    Add files via upload

---
+++ bar.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <h1 @click="changeContent">{{content}}</h1>
+    </header>
+
+
+<main>
+</main>
+
+</template>
+
+<script lang="ts">
+    export default
+    data(){
+        return {
+            content: 'WEB'
+        }
+    },
+methods: {
+    changeContent() {
+        this.content = "strona"
+    }
+}
+
+
+
+</script>
